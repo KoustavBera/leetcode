d46bbf32edec375d305c3e49ca770fddde325dc9
Koustav Bera <143155312+KoustavBera@users.noreply.github.com>
2026-07-12 22:30:32 +0530

    Time: 1 ms (99.92%), Space: 50.2 MB (55.57%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -11,17 +11,16 @@ for(int i=0; i<m; i++){
 Arrays.fill(dp[i], -1);
 
 }
-return f(grid, m-1, n-1, dp, 0);
+return f(grid, m-1, n-1, dp);
     }
-    int f(int[][]grid, int i, int j, int[][] dp, int ans){
+    int f(int[][]grid, int i, int j, int[][] dp){
         if(i==0 && j==0) return grid[0][0];
         if(i<0 || j<0) return Integer.MAX_VALUE; 
         if(dp[i][j] != -1) return dp[i][j];
 
-        int up = f(grid, i-1, j, dp, ans);
-        int left = f(grid, i, j-1, dp , ans);
+        int up = f(grid, i-1, j, dp);
+        int left = f(grid, i, j-1, dp);
 
-        ans +=  (int) Math.min(up, left) + grid[i][j];
-        return dp[i][j] = ans;
+        return dp[i][j] = Math.min(up, left) + grid[i][j];
     }
 }
