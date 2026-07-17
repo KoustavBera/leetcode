41f573f29239f16c34d18026410b021a65163fd7
Koustav Bera <143155312+KoustavBera@users.noreply.github.com>
2026-07-18 01:09:08 +0530

    Time: 5 ms (35.48%), Space: 46.7 MB (7.91%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -6,7 +6,9 @@ class Solution {
         for(int s=0; s<i; s++){
         Arrays.fill(dp[s], Integer.MAX_VALUE);
         }
-        return f(triangle, 0, 0, i,  dp);
+        // return f(triangle, 0, 0, i,  dp);
+        //tabulation
+        return f2(triangle);
     }
     int f(List<List<Integer>>  triangle,int i, int j, int n, int[][]dp){
         //base case : if i am in n-1 row return 
@@ -17,6 +19,22 @@ class Solution {
         int dg = triangle.get(i).get(j) + f(triangle, i+1, j+1, n, dp);
 
         return dp[i][j] = Math.min(d, dg);
+    }
+
+    int f2(List<List<Integer>>  triangle){
+        int n = triangle.size();
+        int[][] dp = new int[n][triangle.get(n-1).size()];
+        for(int i=0; i<triangle.get(n-1).size(); i++){
+            dp[n-1][i] = triangle.get(n-1).get(i);
+        }
+        for(int i=triangle.size()-2; i>=0; i--){
+            for(int j=i; j>=0; j--){
+               int d = triangle.get(i).get(j) + dp[i+1][j];
+                int dg = triangle.get(i).get(j) + dp[i+1][j+1];
+                dp[i][j] = Math.min(d, dg);
+            }
+        }
+        return dp[0][0];
     }
 
 }
