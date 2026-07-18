46d7b22a06d0c10b4a67929b76d157d266c35288
Koustav Bera <143155312+KoustavBera@users.noreply.github.com>
2026-07-18 17:16:31 +0530

    Time: 1 ms (99.9%), Space: 55.9 MB (6.64%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -8,6 +8,7 @@ class Solution {
         // use HashMap (O^n)
 
         //Optimal (Boyer-Moore Voting)
+        //logic - 2 2 2 2 1 1 1 -> 2+ -> 2++ -> 2+++ -> 2++++ -> 2+++ -> 2++ ->2+ return 2, reset only when count = 0 (draw/tie)
         int count = 0;
         int candidate = 0;
         for(int num: nums){
