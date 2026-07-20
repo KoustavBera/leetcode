19e8974abb8b4b6554f52985f12c91b8c3230565
Koustav Bera <143155312+KoustavBera@users.noreply.github.com>
2026-07-21 04:58:31 +0530

    Time: 14 ms (92.21%), Space: 51.7 MB (6.7%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int l=0, r=0;
+        int count = 0;
+        while(l<g.length && r<s.length){
+            if(g[l]<=s[r]){ //greed <= size
+                count++; //increase count
+                l++;
+            }
+            
+            r+=1; //satisfied or not move to next ccookie
+        }
+        return count;
+    }
+}
