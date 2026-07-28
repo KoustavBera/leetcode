a8fc71cbc7d511c022c3bcd44e765e27e0aa26f9
Koustav Bera <143155312+KoustavBera@users.noreply.github.com>
2026-07-28 12:26:51 +0530

    Time: 2 ms (99.24%), Space: 119.6 MB (9.91%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -11,12 +11,13 @@ class Solution {
         int current=0, total=0, station = 0;
         for(int i=0 ; i < gas.length; i++){
             //current is gas - journey
-            current += gas[i] - cost[i];
-            total += current;
+            int netGas =  gas[i] - cost[i];;
+            current += netGas;
+            total += netGas;
             // if current < 0 , joruney not feasible update current to 0 and move to next station
             if(current<0){
                 current = 0;
-                station += 1;
+                station = i + 1;
             }
         }
         //at end check if total < 0 or not
