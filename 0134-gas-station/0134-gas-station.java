99e96fd43087c8b3ff6a69ea6aceb4f26363110e
Koustav Bera <143155312+KoustavBera@users.noreply.github.com>
2026-07-28 12:23:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,25 +1,25 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-      int totalTank = 0;
-        int currentTank = 0;
-        int startStation = 0;
-        
-        for (int i = 0; i < gas.length; i++) {
-            // 1. Calculate the net gas at station i
-            int netGas  = gas[i] - cost[i];
-            // 2. Add it to totalTank and currentTank
-            currentTank += netGas;
-            totalTank += netGas;
-            // 3. If currentTank drops below 0, what do we do? (Think about your pointers and res
-            if(currentTank < 0){
-                currentTank = 0;
-                startStation = i+1;
+        // logic is fairly simple
+        // suppose station i has petrol 2 ltr now the next journey will require
+        // petrol 4 ltr
+        //so the question is is the journey possible? No ofcourse, so station ++;
+        //subsequently we have to check even if we found a station is the whole
+        // journey possible or not
+        //for the whole jounrey to be possible summation(gas[i] - cost[i]) >=0
+        //thats why in the mean time we also track total. if total <0, return  -1
+        int current=0, total=0, station = 0;
+        for(int i=0 ; i < gas.length; i++){
+            //current is gas - journey
+            current += gas[i] - cost[i];
+            total += current;
+            // if current < 0 , joruney not feasible update current to 0 and move to next station
+            if(current<0){
+                current = 0;
+                station += 1;
             }
         }
-        return (totalTank<0)?-1:startStation;
-        
-        // 4. Check the global condition at the end
-        
-        // 5. Return the answer
+        //at end check if total < 0 or not
+        return (total < 0 )? -1 : station ;
     }
 }
