32fb42388c5a0b39eb66e33b8b0f958f74553d9f
Koustav Bera <143155312+KoustavBera@users.noreply.github.com>
2026-07-28 13:40:53 +0530

    Time: 2 ms (88.86%), Space: 47.7 MB (92.16%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,10 +1,14 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int reachable=0;
-        for(int i=0; i<nums.length; i++){
-           if(i > reachable) return false;
-           //if i<reachable, check reachable
-           reachable = Math.max(reachable, i + nums[i]);
+        // logic is fairly simple
+        // at each index we cal the max reachable index from that index.
+        // if at any point current index > what we assumed to be max reachable index
+        // viola! return false;
+        // else return true;
+        int reachable = 0;
+        for(int i=0; i< nums.length; i++){
+            if(i > reachable) return false;
+            reachable = Math.max(reachable, i+nums[i]);
         }
         return true;
     }
