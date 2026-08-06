bada027f7efcfe79ff40943d32e6986cafc43558
Koustav Bera <143155312+KoustavBera@users.noreply.github.com>
2026-08-06 20:12:57 +0530

    Time: 353 ms (64.43%), Space: 0B (100%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -1,10 +1,19 @@
 # Write your MySQL query statement below
+-- My approch
+-- select e.employee_id
+-- from Employees e
+-- where e.salary<30000 and not exists
+-- (
+--     select 1 from Employees m
+--     where  e.manager_id = m.employee_id
+-- )
+-- except
+-- select e.employee_id from Employees e where e.salary<30000 and e.manager_id is null order by employee_id
+
 select e.employee_id
 from Employees e
-where e.salary<30000 and not exists
+where e.salary<30000 and e.manager_id is not null and not exists
 (
     select 1 from Employees m
     where  e.manager_id = m.employee_id
-)
-except
-select e.employee_id from Employees e where e.salary<30000 and e.manager_id is null order by employee_id
+) order by e.employee_id
