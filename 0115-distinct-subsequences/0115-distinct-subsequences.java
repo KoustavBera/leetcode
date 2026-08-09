f2c649489e982037baf44154b40fc2962645f839
Koustav Bera <143155312+KoustavBera@users.noreply.github.com>
2026-08-10 01:14:22 +0530

    Time: 24 ms (15.12%), Space: 63.5 MB (11.31%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -2,8 +2,9 @@ class Solution {
     public int numDistinct(String s, String t) {
         int[][]dp = new int[s.length()+1][t.length()+1];
         for(int[] row:dp) Arrays.fill(row, -1);
-        return f2(s,t, s.length()-1, t.length()-1, dp);
+        return f3(s,t);
     }
+
     //normal rec
     int f(String s, String t, int i, int j){
         if(j<0) return 1;
@@ -17,7 +18,7 @@ class Solution {
         }
 
     }
-    //Tabulation
+    //Memo
     int f2(String s, String t, int i, int j, int [][]dp){
         if(j<0) return 1;
         if(i<0) return 0;
@@ -28,6 +29,29 @@ class Solution {
         else{
             return dp[i][j] = f2(s,t, i-1, j,dp);
         }
-
     }
+
+    //Tabulation
+        int f3(String s, String t){
+        int[][]dp = new int[s.length()+1][t.length()+1];
+        //here we cant take -1 as a index to check so we will use 1 based indexing
+        // if(j<0) return 1;
+        for(int i=0; i<s.length(); i++){
+            dp[i][0] = 1;
+        }
+        //it becomes
+        for(int i=1; i<=s.length(); i++ ){
+            for(int j=1; j<=t.length(); j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+//  dp[i][j] = f2(s, t, i-1, j-1, dp) + f2(s, t, i-1, j, dp);
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }
+                else
+                dp[i][j] = dp[i-1][j];
+        }
+            }
+        return dp[s.length()][t.length()];
+    
+    }
+
 }
