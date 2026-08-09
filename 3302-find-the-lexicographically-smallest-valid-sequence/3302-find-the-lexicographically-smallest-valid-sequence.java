4f98b418692dfdb3ba1d0f305823c9c66028d7b3
Koustav Bera <143155312+KoustavBera@users.noreply.github.com>
2026-08-09 15:16:13 +0530

    Time: 40 ms (13.16%), Space: 135.8 MB (100%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.java
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.java
@@ -1,47 +1,42 @@
 class Solution {
     public int[] validSequence(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-
-        int[] last = new int[m];
-
-        java.util.Arrays.fill(last, -1);
-
-        int i = n - 1;
-        int j = m - 1;
-
-        while (i >= 0 && j >= 0) {
-            if (word1.charAt(i) == word2.charAt(j)) {
-                last[j] = i;
-                --j;
-            }
-
-            --i;
+       int i=0, j=0, k = 0, used = 0;
+       int[] arr = new int[word2.length()];
+       int[] rightHandSideMatch = new int[word1.length()+1];
+       i = word1.length()-1;
+       j = word2.length()-1;
+       int rightMatched = 0;
+       while(i>=0){
+        if(j>=0 && word1.charAt(i) == word2.charAt(j)){
+            rightMatched +=1;
+            j--;
         }
-
-        int[] ans = new int[m];
-        int size = 0;
-
-        boolean canSkip = true;
-        j = 0;
-
-        for (i = 0; i < n && j < m; ++i) {
-            if (word1.charAt(i) == word2.charAt(j)) {
-                ans[size++] = i;
-                ++j;
+        rightHandSideMatch[i] = rightMatched;
+        i--;
+       }
+        i=0; j=0;
+        while(i<word1.length() && j<word2.length()){
+            char c1 = word1.charAt(i);
+            char c2 = word2.charAt(j);
+            if(c1 == c2){
+                arr[k++]=i;
+                i++;
+                j++;
             }
-            else if (canSkip &&
-                     (j == m - 1 || i < last[j + 1])) {
-                canSkip = false;
-                ans[size++] = i;
-                ++j;
+            if(c1!=c2){
+                //check if we can transform
+                if(used==0 &&  rightHandSideMatch[i+1] >= word2.length()-j-1){
+                    used = 1;
+                    arr[k++] = i;
+                    i++;
+                    j++;
+                }
+                else{
+                    //used already so just move the word1 pointer
+                    i++;
+                }
             }
         }
-
-        if (j == m) {
-            return ans;
-        }
-
-        return new int[0];
+        return j==word2.length() ? arr : new int[0];
     }
 }
