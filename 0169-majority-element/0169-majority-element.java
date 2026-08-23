fffbb6aa8f843ade83e55fce6443b076964cf6ae
Koustav Bera <143155312+KoustavBera@users.noreply.github.com>
2026-08-24 01:11:47 +0530

    Time: 2 ms (70.98%), Space: 55.7 MB (31.98%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -9,11 +9,12 @@ class Solution {
 
         //Optimal (Boyer-Moore Voting)
         //logic - 2 2 2 2 1 1 1 -> 2+ -> 2++ -> 2+++ -> 2++++ -> 2+++ -> 2++ ->2+ return 2, reset only when count = 0 (draw/tie)
-        int count = 0;
-        int candidate = 0;
-        for(int num: nums){
-            if(count == 0) candidate = num;
-            count += (candidate == num )? 1 : -1;
+        int count = 0, candidate = 0;
+        for(int num : nums){
+            if(count == 0){
+                candidate = num;
+            }
+                count += (candidate == num)?1:-1;
         }
         return candidate;
     }
