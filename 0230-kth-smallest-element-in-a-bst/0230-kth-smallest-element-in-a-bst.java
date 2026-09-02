29d6198b103d9bf5a060805b6cb0dcae558037a8
Koustav Bera <143155312+KoustavBera@users.noreply.github.com>
2026-09-03 01:03:24 +0530

    Time: 0 ms (100%), Space: 47 MB (17.67%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -13,21 +13,21 @@
  *     }
  * }
  */
-
 class Solution {
-    int i=1;
+    int i = 1;
     int result = 0;
     public int kthSmallest(TreeNode root, int k) {
-         inorder(root, k );
-         return result;
+        inorder(root, k);
+        return result;
     }
-    void inorder(TreeNode root,int k){
+    public void inorder(TreeNode root, int k){
         if(root == null) return;
-        inorder(root.left,k);
-        if(i++ == k){
-              result = root.val ;
-              return;
-        } 
-        inorder(root.right,k);
+
+        inorder(root.left, k);
+        if(i++ == k) {
+            result = root.val;
+            return;
+        }
+        inorder(root.right, k);
     }
 }
